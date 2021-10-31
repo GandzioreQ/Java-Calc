cf15e749fce440ffefdaca4aea228f352aa0c47a
danie <danie@DESKTOP-F4542VG.home>
2021-11-01 00:14:51 +0100

    Third step of my credit calculator. Validation added + messages

--- calc_cred_01/src/main/java/credit_calc/CreditBB.java
+++ calc_cred_01/src/main/java/credit_calc/CreditBB.java
@@ -2,70 +2,64 @@ package credit_calc;
 
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.application.FacesMessage;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.lang.Math;
+
 
 @Named
 @RequestScoped
 
 public class CreditBB {
 	
-	private String amount;
-	private String period;
-	private String rate;
+	private double amount;
+	private double period;
+	private double rate;
 	private double result;
 
 	@Inject
 	FacesContext ctx;
 
-	public String getAmount() {
+	public double getAmount() {
 		return amount;
 	}
 	
-	public void setAmount(String amount) {
+	public void setAmount(double amount) {
 		this.amount = amount;
 	}
 	
-	public String getPeriod() {
+	public double getPeriod() {
 		return period;
 	}
 
-	public void setPeriod(String period) {
+	public void setPeriod(double period) {
 		this.period = period;
 	}
 
-	public String getRate() {
+	public double getRate() {
 		return rate;
 	}
 
-	public void setRate(String rate) {
+	public void setRate(double rate) {
 		this.rate = rate;
 	}
 
-	public Double getResult() {
+	public double getResult() {
 		return result;
 	}
 
-	public void setResult(Double result) {
+	public void setResult(double result) {
 		this.result = result;
 	}
 	
 	
 	public void countRate() {
-		if (amount == "" || period == "" || rate == "") {
-			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Please enter all data", null));
-		} else {
-			double amount = Double.parseDouble(this.amount);
-			double period = Double.parseDouble(this.period);
-			double rate = Double.parseDouble(this.rate);
-			
+		
 			double rate_number = rate / 100; 
 			double period_number = period * 12;
 			System.out.print(rate_number);
 			double q = 1 + (rate_number / 12);
 			result = Math.round(amount * Math.pow(q, period_number) * (q - 1) / (Math.pow(q, period_number)-1));
-		}
+		
 	}
 }
